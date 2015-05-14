e5bc4775306e2abec1daf63c6c6875b417ff6c60
Nicholas Valbusa <squallstar@gmail.com>
2015-05-14 12:54:16 +0100

    database configuration

---
+++ core/models/Config.js
@@ -0,0 +1,18 @@
+var Sequelize = require('sequelize');
+
+var database = require('../libs/database');
+
+var Config = database.define('config', {
+  key: {
+    type: Sequelize.STRING,
+    field: 'configKey'
+  },
+  value: {
+    type: Sequelize.STRING,
+    field: 'configValue'
+  }
+}, {
+  freezeTableName: true
+});
+
+module.exports = Config;
