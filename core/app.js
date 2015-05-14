af101b653cde8022be84e63235475eb1f3d7ee3f
Nicholas Valbusa <squallstar@gmail.com>
2015-05-14 11:36:02 +0100

    application skeleton

---
+++ core/app.js
@@ -0,0 +1,82 @@
+/*
+@package   Bancha.js
+@author    Nicholas Valbusa
+@copyright Nicholas Valbusa 2015(C)
+
+---------------------------
+
+Application bootstrap file
+
+*/
+
+// promises for everybody!
+require("native-promise-only");
+
+// core libs
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// framework libs
+var config = require('./libs/config').options();
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.locals.build = Math.round(Date.now()/1000);
+
+// pretty views on local
+if(config.env === 'local') {
+  app.locals.pretty = true;
+  app.disable('view cache');
+}
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// application routes
+app.use('/', require('./routes/index'));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
---
+++ core/routes/index.js
@@ -0,0 +1,23 @@
+/*
+@package   Bancha
+@author    Nicholas Valbusa
+@copyright Nicholas Valbusa 2015(C)
+
+---------------------------
+
+Index router
+
+*/
+
+var express = require('express');
+var router = express.Router();
+
+var config = require('../libs/config').options();
+
+// -------------------------------------------------------
+
+// TODO
+
+// -------------------------------------------------------
+
+module.exports = router;
